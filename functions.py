e14c12a9a7190ad61a530705ee5d71658d31c223
W. Milczarek <wmilczarek@gmail.com>
2022-03-24 23:08:54 +0100

    Modified the function which gets current word meaning from the Internet. Now it modifies i tag, so that can be used in markup and span tag is removed completely from source text. When there's no internet connection, the app just doesn't display any text.

--- functions.py
+++ functions.py
@@ -21,46 +21,60 @@ def words_dictionary(name):
 
 def get_dictionary_entry(name):
     cleared_meainings = ""
-    entry_address = f"https://sjp.pwn.pl/szukaj/{name}.html"
-    entry_page=requests.get(entry_address)
-    soup = bs(entry_page.content,'html.parser')
-    #print(soup.prettify)
-    meanings = soup.find_all("div",{"class":"znacz"})
-    text_meanings = str(meanings)
-    if len(text_meanings)!=2:
-        text_meanings = text_meanings.split("«")
-        counter = 0
-        for item in text_meanings:
-            if "»" not in item:
-                text_meanings.pop(counter)
-                counter+=1
-        counter = 1
-        for split_item in text_meanings:
-            if ("»</div>") in split_item:
-                split_item = split_item.split("»</div>")
-                new_meaining = f"[b]{counter}.[/b] {split_item[0]}"
-                cleared_meainings = cleared_meainings + new_meaining + "\n"
-                counter +=1
-        #print(cleared_meainings) 
-    else:
-        meanings = soup.find_all("div",{"class":"ribbon-element"})
+    try:
+        entry_address = f"https://sjp.pwn.pl/szukaj/{name}.html"
+        entry_page=requests.get(entry_address)
+        soup = bs(entry_page.content,'html.parser')
+        #print(soup.prettify)
+        meanings = soup.find_all("div",{"class":"znacz"})
         text_meanings = str(meanings)
         if len(text_meanings)!=2:
             text_meanings = text_meanings.split("«")
-            text_meanings = text_meanings[1].split("»")
-            cleared_meainings = "[b]1.[/b] " + text_meanings[0]
+            counter = 0
+            for item in text_meanings:
+                if "»" not in item:
+                    text_meanings.pop(counter)
+                    counter+=1
+            counter = 1
+            for split_item in text_meanings:
+                if ("»</div>") in split_item:
+                    split_item = split_item.split("»</div>")
+                    if "<" in split_item[0]:
+                        #print(split_item[0])
+                        split_item[0] = split_item[0].replace("<","[")
+                        split_item[0] = split_item[0].replace(">","]")
+                        if "span" in split_item[0]:
+                            split_item[0] = split_item[0].replace("[/span]","")
+                            split_item[0] = split_item[0].replace('[span class="skrot-inny"]',"")
+                        #print(split_item[0])
+                    new_meaining = f"[b]{counter}.[/b] {split_item[0]}"
+                    cleared_meainings = cleared_meainings + new_meaining + "\n"
+                    counter +=1
+            #print(cleared_meainings) 
         else:
-            print('nothing found')
-
+            meanings = soup.find_all("div",{"class":"ribbon-element"})
+            text_meanings = str(meanings)
+            if len(text_meanings)!=2:
+                text_meanings = text_meanings.split("«")
+                text_meanings = text_meanings[1].split("»")
+                cleared_meainings = "[b]1.[/b] " + text_meanings[0]
+            else:
+                print('nothing found')
+    except:
+        print("no internet")
+        pass
     return(cleared_meainings)
 
 #get_dictionary_entry("tutaj")
 
 """ dictio  = words_dictionary("correct_answers")
 counter = 1
-for i in range(0,len(dictio)):
-    print(dictio[i])
-    print(get_dictionary_entry(dictio[i]))
+for i in range(0,3):
+    print(counter)
+    dict_def = get_dictionary_entry(dictio[i])
+    if "[span" in dict_def:
+        print(dictio[i])
+        print(dict_def)
+    #print(get_dictionary_entry(dictio[i]))
     counter+=1
- """
-
+print('end') """
